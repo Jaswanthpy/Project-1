6e730b25160051453ca57e710fdbea990f59f3fe
Jaswanthpy <60251024+Jaswanthpy@users.noreply.github.com>
2021-06-07 18:18:51 +0530

    Update source_code_hash

--- lambda.tf
+++ lambda.tf
@@ -47,7 +47,7 @@ resource "aws_lambda_function" "test_lambda" {
   function_name = "test_lambda"
   role = aws_iam_role.iam_for_lambda.arn
   handler = "hello.hello"
-  source_code_hash = filebase64sha256("init.zip")
+  source_code_hash = data.archive_file.init.output_base64sha256
   runtime = "python3.8"
   depends_on = [
   aws_iam_role_policy.cloudwatch_lambda,
